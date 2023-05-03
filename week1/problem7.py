e6df0e761af6670a31fee770e2049251415f8b2e
Linh Toan <linh.p.toan@gmail.com>
2023-05-03 15:51:53 -0500

    Update problem7.py

--- week1/problem7.py
+++ week1/problem7.py
@@ -14,8 +14,7 @@ def solution(df):
 
     # Part 4: Get the underlying NumPy array. Let's call it "X"
     X = lotus_df.to_numpy()
-
-<<<<<<< HEAD
+    
     # Part 5: Compute matrix-matrix multiplication between the transpose of X and X. 
     # To get the transpose, use X.T. Let's call the result XTX.
     XTX = X.T @ X
@@ -30,18 +29,3 @@ def solution(df):
     w = (XTX_inv @ X.T) @ y
     return w[0].round(3)
     # 4.594
-=======
-    #Question 5: Compute matrix-matrix multiplication between the transpose of X and X. 
-    # To get the transpose, use X.T. Let's call the result XTX.
-    XTX = X.T @ X
-
-    # Question 6: Compute the inverse of XTX
-    XTX_inv = np.linalg.inv(XTX)
-
-    # Question 7: Create an array y with values [1100, 800, 750, 850, 1300, 1000, 1000, 1300, 800].
-    y = np.array([1100, 800, 750, 850, 1300, 1000, 1000, 1300, 800])
-
-    # Question 8: Multiply the inverse of XTX with the transpose of X, and then multiply the result by y. Call the result w.
-    w = (XTX_inv @ X.T) @ y
-    return w[0].round(3)
->>>>>>> main
