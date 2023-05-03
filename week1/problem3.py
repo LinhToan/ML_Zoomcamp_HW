658e8d84f90d1c8755545f6f3a5f3ba0c08bfaf4
Linh Toan <linh.p.toan@gmail.com>
2023-05-03 15:48:53 -0500

    Update problem3.py

--- week1/problem3.py
+++ week1/problem3.py
@@ -2,9 +2,5 @@
 import pandas as pd
 
 def solution(df):
-<<<<<<< HEAD
-    return df.Make.value_counts().head(3)
-=======
     return df.Make.value_counts().head(3).to_dict()
->>>>>>> main
     # Top 3 most frequent car manufacturers are Chevrolet, Ford, and Volkswagen.
