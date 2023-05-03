de09778c9307c4f62d890d03bbd11c15c93bd006
Linh Toan <linh.p.toan@gmail.com>
2023-05-03 15:44:47 -0500

    Update test7.py

--- week1/__tests__/test7.py
+++ week1/__tests__/test7.py
@@ -5,10 +5,6 @@ sys.path.insert(0, '../')
 from problem7 import solution
 
 class Test7(unittest.TestCase):
-<<<<<<< HEAD
-=======
-    
->>>>>>> main
     def test_7(self):
         df = pd.read_csv(r'../data.csv')
         self.assertEqual(solution(df), 4.595)
