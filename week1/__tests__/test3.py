e50e1420179e23e8a629581f9a75b1b518d7cc4e
Linh Toan <linh.p.toan@gmail.com>
2023-05-03 15:46:49 -0500

    Update test3.py

--- week1/__tests__/test3.py
+++ week1/__tests__/test3.py
@@ -7,7 +7,6 @@ from problem3 import solution
 class Test3(unittest.TestCase):
     def test_3(self):
         df = pd.read_csv(r'../data.csv')
-<<<<<<< HEAD
         self.assertEqual('Chevrolet' in solution(df), True)
         self.assertEqual('Ford' in solution(df), True)
         self.assertEqual('Volkswagen' in solution(df), True)
@@ -17,9 +16,3 @@ class Test3(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-=======
-        self.assertEqual(solution(df), df.Make.value_counts().head(3).to_dict())
-
-if __name__ == '__main__':
-    unittest.main()
->>>>>>> main
