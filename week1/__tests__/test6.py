809c001d3446011d6023ae52eb1ecbd77c6ad4b2
Linh Toan <linh.p.toan@gmail.com>
2023-05-03 15:48:03 -0500

    Update test6.py

--- week1/__tests__/test6.py
+++ week1/__tests__/test6.py
@@ -2,7 +2,6 @@ import sys
 import unittest
 import pandas as pd
 sys.path.insert(0, '../')
-<<<<<<< HEAD
 from problem6 import solutionA, solutionB, solutionC
 df = pd.read_csv(r'../data.csv')
 
@@ -15,23 +14,6 @@ class Test6(unittest.TestCase):
 
     def test_6C(self):
         self.assertEqual(solutionC(df), 6)
-=======
-from problem6 import solution1, solution2, solution3, solution4
-df = pd.read_csv(r'../data.csv')
-class Test6(unittest.TestCase):
-    
-    def test_6(self):
-        self.assertEqual(solution1(df), 6)
-    
-    def test_6b(self):
-        self.assertEqual(solution2(df), 4)
-
-    def test_6c(self):
-        self.assertEqual(solution3(df), 0)
-    
-    def test_6d(self):
-        self.assertEqual(solution4(df), 6)
->>>>>>> main
 
 if __name__ == '__main__':
     unittest.main()
