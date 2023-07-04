cbef322eb02c55b2a991097493723109f2f98a75
LinhToan <linh.p.toan@gmail.com>
2023-07-04 11:10:03 -0500

    solves problem 4 and creates its test file

--- week5/__tests__/test4.py
+++ week5/__tests__/test4.py
@@ -8,10 +8,10 @@ class Test4(unittest.TestCase):
     def test_4(self):
         model_file = '../model1.bin'
         dv_file = '../dv.bin'
-        url = "http://localhost:9696/problem4"
+        url = "http://0.0.0.0:9696/problem4"
         client = {"reports": 0, "share": 0.245, "expenditure": 3.438, "owner": "yes"}
         response = requests.post(url, json=client).json()
-        self.assertEqual(solution(model_file, dv_file, client), 0.162)
+        self.assertEqual(solution(model_file, dv_file, client), 0.928)
 
 if __name__ == '__main__':
     unittest.main()
--- week5/problem4.py
+++ week5/problem4.py
@@ -1,35 +1,31 @@
+# To run this flask application, use `pipenv shell` and 
+# then `gunicorn --bind 0.0.0.0:9696 problem4:app`, then finally run test4.py
 import pickle
 from flask import Flask
 from flask import request
 from flask import jsonify
 
-def load(filename):
+def load(filename: str):
     with open(filename, 'rb') as f_in:
         return pickle.load(f_in)
 
+dv = load('./dv.bin')
+model = load('./model1.bin')
+
 app = Flask('problem4')
 
-def predict(model_file, dv_file, client):
-    model = load(model_file)
-    dv = load(dv_file)
-
-    X = dv.transform([client])
-    y_pred = round(model.predict_proba(X)[0,1], 3)
-
-    return y_pred
-
 @app.route('/problem4', methods=['POST'])
-def solution(model_file, dv_file, client):
-
+def solution():
     client = request.get_json()
 
-    y_pred = predict(model_file, dv_file, client)
-
+    X = dv.transform([client])
+    y_pred = round(model.predict_proba(X)[0, 1], 3)
+ 
     result = {
-        "credit approval probability": float(y_pred)
+        'credit_approval_probability': float(y_pred)
     }
 
     return jsonify(result)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
